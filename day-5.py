5c3e7ca24630a6687cad0cdade022e16d6c2f323
Om Verma <omverma1234q@gmail.com>
2022-12-08 20:27:53 +0530

    Day-5

---
+++ day-5.py
@@ -0,0 +1,8 @@
+#welcome to python day-5
+'''
+hey there
+I am using multi-line comments
+'''
+print("This person is a \na good boy")
+print("This guy is a \"handsome guy\" and muscular as well")
+print(18, 10, sep="-", end="-2003")
