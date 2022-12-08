25249ba936c998807e436a0a025c17ea7de057b4
Om Verma <omverma1234q@gmail.com>
2022-12-08 20:28:20 +0530

    Day-6

---
+++ day-6.py
@@ -0,0 +1,21 @@
+a = 1
+print(a)
+b = "om"
+print(b)
+c = True
+print(c)
+d = None
+print(d)
+print("The type of d is", type(d))
+e = 124
+print(e)
+f = 1.423
+print(f)
+g = complex(2 + 3)
+print(g)
+list1 = [8, 2.3, ["om", "verma"], [-4, 5]]
+print(list1)
+tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))
+print(tuple1)
+dict1 = {"name": "om", "age": 20}
+print(dict1)
